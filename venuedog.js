18bee5c92541b78da738fdef79538b99fec95bc2
Richard <richard@saidigital.co>
2012-03-29 16:16:51 -0400

    Started iterative approach to displaying events by week.

--- venuedog.js
+++ venuedog.js
@@ -6,9 +6,12 @@
  *  @license GPL 2
  */
 (function( $ ) {
+
+
   $.fn.venuedog = function(args) {
 
     var event_wrapper = $(this);
+    var selector = this.selector;
 
 
     /* Override defaults with arguments and convert to query string*/
@@ -32,30 +35,50 @@
     var events_url = src + "woof/events/show_by_date?" + query.join("");
 
 
+    /* Initialize Events by Day display */
+    if(settings.list_by == "day"){
+      list = get_week(src, events_url, event_wrapper);
+
+      /* Bind next and previous buttons to actions */
+      $(selector + " a.next_events").click( function(e){
+        e.preventDefault();
+        show_week();
+      });
+    }
+
+
+  };
+
+
+
+  /* Events Grouped by Day */
+  var get_week = function(src, events_url, event_wrapper) {
+
     /* Get Data from VenueDog.com */
     $.getJSON(events_url + "&callback=?", function(data){
+      tmp = '<ul>';
 
-      list += '<ul>';
-      $.each(data, function(date, group){
+      $.each(data, function(the_date, group){
+        day = new Date(the_date);
+        tmp += "<li>" + day.toLocaleDateString() + "<ul>";
 
-        /* Events Grouped by Day */
-        if(settings.list_by == "day"){
-          day = new Date(date);
-          list += "<li>" + day.toLocaleDateString() + "<ul>";
-          $.each(group, function(j, ev){
+        $.each(group, function(j, ev){
             event_url = src + "events/" + ev.id;
             event_link = '<a href="'+ event_url + '" target="_blank">' + ev.name +'</a>';
             out = event_link + " @ " + ev.venue_name + " (" + ev.start_time + ")";
-            list += "<li>" + out + "</li>";
-          });
-          list += "</ul></li>";
-        }
-
+            tmp += "<li>" + out + "</li>";
+        });
+        tmp += "</ul></li>";
       });
-      list += '</ul>';
 
-      $(event_wrapper).html(list);
+      tmp += '<li><a class="next_events" href="#next">Next</a></li>';
+      tmp += '</ul>';
+
+      $(event_wrapper).html(tmp);
     });
 
-  };
+  }
+
+
+
 })( jQuery );
