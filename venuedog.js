3a4594e555e7285e1e8b5979809952f7c2d1b86e
Richard <richard@saidigital.co>
2012-04-06 13:50:59 -0400

    Added arguments for pagination.

--- venuedog.js
+++ venuedog.js
@@ -17,7 +17,11 @@
     /* Override defaults with arguments and convert to query string*/
     var t0 = new Date();
     var t1 = new Date();
-    t1.setDate(t1.getDate() + 7);
+    tmp_paginate = args.paginate > 0 ? args.paginate : 7;
+    console.log(args);
+    t1.setDate(t1.getDate() + tmp_paginate);
+    console.log(t0);
+    console.log(t1);
     var src = "http://venuedog.com/";
     var list = '';
 
@@ -25,8 +29,11 @@
       'start_date' : t0.getFullYear() + "-" + (t0.getMonth() + 1) + "-" + t0.getDate(),
       'end_date'   : t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate(),
       'categories' : "1,2,4,7",
+      'paginate' : "7",
       'list_by'    :  "day"
     }, args);
+
+    console.log(settings);
 
     events_url = build_events_url(src, settings);
 
@@ -70,13 +77,13 @@
       $(selector + ' a.next_events').click( function(e){
         e.preventDefault();
         t0 = new Date(settings.start_date);
-        t0.setDate(t0.getDate() + 7);
+        t0.setDate(t0.getDate() + parseInt(settings.paginate));
         t1 = new Date(settings.end_date);
-        t1.setDate(t1.getDate() + 7);
+        t1.setDate(t1.getDate() + parseInt(settings.paginate));
         sd = t0.getFullYear() + "-" + (t0.getMonth() + 1) + "-" + t0.getDate();
         ed = t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate();
-        settings.start_date = t0;
-        settings.end_date = t1;
+        settings.start_date = t0.toString("yyyy-M-d");
+        settings.end_date = t1.toString("yyyy-M-d");
         new_events_url = build_events_url(src, settings);
 
         load_week(src, new_events_url, event_wrapper, settings, selector);
@@ -84,13 +91,13 @@
       $(selector + ' a.prev_events').click( function(e){
         e.preventDefault();
         t0 = new Date(settings.start_date);
-        t0.setDate(t0.getDate() - 7);
+        t0.setDate(t0.getDate() - settings.paginate);
         t1 = new Date(settings.end_date);
-        t1.setDate(t1.getDate() - 7);
+        t1.setDate(t1.getDate() - settings.paginate);
         sd = t0.getFullYear() + "-" + (t0.getMonth() + 1) + "-" + t0.getDate();
         ed = t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate();
-        settings.start_date = t0;
-        settings.end_date = t1;
+        settings.start_date = t0.toString("yyyy-M-d");
+        settings.end_date = t1.toString("yyyy-M-d");
         new_events_url = build_events_url(src, settings);
 
         load_week(src, new_events_url, event_wrapper, settings, selector);
@@ -108,6 +115,7 @@
       query.push("&" + key + "=" + val);  
     });
     events_url = src + "woof/events/show_by_date?" + query.join("");
+    console.log(events_url);
     return events_url;
   }
 
