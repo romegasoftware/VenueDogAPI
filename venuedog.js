0befd5f4325bc634da2930be936dc2e346e86951
Richard <richard@saidigital.co>
2012-04-09 12:39:10 -0400

    Added function parameters to settings object for improved efficiency.

--- venuedog.js
+++ venuedog.js
@@ -15,26 +15,27 @@
 
 
     /* Override defaults with arguments and convert to query string*/
-    var t0 = new Date();
-    var t1 = new Date();
-    tmp_paginate = args.paginate > 0 ? args.paginate : 7;
-    t1.setDate(t1.getDate() + tmp_paginate);
-    var src = "http://venuedog.com/";
-    var list = '';
-
+    
     var settings = $.extend({
-      'start_date' : t0.getFullYear() + "-" + (t0.getMonth() + 1) + "-" + t0.getDate(),
-      'end_date'   : t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate(),
-      'categories' : "1,2,4,7",
-      'paginate' : "7",
-      'list_by'    :  "day"
+        'categories' : "1,2,4,7",
+        'paginate'   : "7",
+        'list_by'    :  "day",
+        'src'        :  "http://venuedog.com/",
+        'selector'   : this.selector
     }, args);
 
-    events_url = build_events_url(src, settings);
+    var t0 = new Date(); var t1 = new Date();
+    t1.setDate(t1.getDate() + settings.paginate);
+    settings.start_date = t0.getFullYear() + "-" + (t0.getMonth() + 1) + "-" + t0.getDate();
+    settings.end_date   = t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate();
+
+    var list = '';
+
+    settings.events_url = build_events_url(settings);
 
     /* Initialize Events by Day display */
     if(settings.list_by == "day"){
-      list = load_week(src, events_url, event_wrapper, settings, selector);
+      list = load_week(settings);
     }
 
   };
@@ -42,10 +43,10 @@
 
 
   /* Events Grouped by Day */
-  var load_week = function(src, events_url, event_wrapper, settings, selector) {
+  var load_week = function( settings ) {
 
     /* Get Data from VenueDog.com */
-    $.getJSON(events_url + "&callback=?", function(data){
+    $.getJSON(settings.events_url + "&callback=?", function(data){
       tmp = '<ul>';
 
       $.each(data, function(the_date, group){
@@ -53,7 +54,7 @@
         tmp += "<li>" + day.addHours(24).toString("ddd, MMM d, yyyy") + "<ul>";
 
         $.each(group, function(j, ev){
-            event_url = src + "events/" + ev.id;
+            event_url = settings.src + "events/" + ev.id;
             event_link = '<a href="'+ event_url + '" target="_blank">' + ev.name +'</a>';
             event_time = new Date(ev.start_time);
             out = event_link + " @ " + ev.venue_name + " (" + event_time.toString("h:mm tt") + ")";
@@ -65,11 +66,11 @@
       tmp += '<li class="cron"><a class="prev_events" href="#previous">Previous</a>&nbsp;<a class="next_events" href="#next">Next</a></li>';
       tmp += '</ul>';
 
-      $(event_wrapper).hide().html(tmp).fadeIn('slow');
+      $(settings.selector).hide().html(tmp).fadeIn('slow');
 
 
       /* Bind next and previous buttons to actions */
-      $(selector + ' a.next_events').click( function(e){
+      $(settings.selector + ' a.next_events').click( function(e){
         e.preventDefault();
         t0 = new Date(settings.start_date);
         t0.setDate(t0.getDate() + parseInt(settings.paginate));
@@ -79,11 +80,12 @@
         ed = t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate();
         settings.start_date = t0.toString("yyyy-M-d");
         settings.end_date = t1.toString("yyyy-M-d");
-        new_events_url = build_events_url(src, settings);
+        settings.events_url = build_events_url(settings);
+        console.log(settings);
 
-        load_week(src, new_events_url, event_wrapper, settings, selector);
+        load_week( settings );
       });
-      $(selector + ' a.prev_events').click( function(e){
+      $(settings.selector + ' a.prev_events').click( function(e){
         e.preventDefault();
         t0 = new Date(settings.start_date);
         t0.setDate(t0.getDate() - settings.paginate);
@@ -93,9 +95,9 @@
         ed = t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate();
         settings.start_date = t0.toString("yyyy-M-d");
         settings.end_date = t1.toString("yyyy-M-d");
-        new_events_url = build_events_url(src, settings);
+        settings.events_url = build_events_url(settings);
 
-        load_week(src, new_events_url, event_wrapper, settings, selector);
+        load_week( settings );
       });
 
     });
@@ -104,13 +106,14 @@
 
 
   /* Build API query string from date data */
-  var build_events_url = function(src, settings){
+  var build_events_url = function(settings){
     var query = [];
     $.each(settings, function(key, val){
       query.push("&" + key + "=" + val);  
     });
-    events_url = src + "woof/events/show_by_date?" + query.join("");
-    return events_url;
+    events_url = settings.src + "woof/events/show_by_date?" + "start_date=" + settings.start_date +
+                 "&end_date=" + settings.end_date + "&categories=" + settings.categories;
+    return encodeURI(events_url);
   }
 
 
