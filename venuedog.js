eee6243b894f9fee837bef54aa75b12963dd80f2
Richard <richard@saidigital.co>
2012-04-09 14:44:11 -0400

    Fixed bug for IE in date parsing with Z defined.

--- venuedog.js
+++ venuedog.js
@@ -17,20 +17,17 @@
     /* Override defaults with arguments and convert to query string*/
     
     var settings = $.extend({
-        'categories' : "1,2,4,7",
+        'categories' : "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17",
         'paginate'   : "7",
         'list_by'    :  "day",
         'src'        :  "http://venuedog.com/",
         'selector'   : this.selector
     }, args);
 
-    var t0 = new Date(); var t1 = new Date();
-    t1.setDate(t1.getDate() + settings.paginate);
-    settings.start_date = t0.getFullYear() + "-" + (t0.getMonth() + 1) + "-" + t0.getDate();
-    settings.end_date   = t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate();
+    settings.start_date = Date.today();
+    settings.end_date   = Date.today().add(parseInt(settings.paginate)).days();
 
     var list = '';
-
     settings.events_url = build_events_url(settings);
 
     /* Initialize Events by Day display */
@@ -50,13 +47,13 @@
       tmp = '<ul>';
 
       $.each(data, function(the_date, group){
-        day = new Date(the_date);
-        tmp += "<li>" + day.addHours(24).toString("ddd, MMM d, yyyy") + "<ul>";
+        day = new Date( Date.parse(the_date));
+        tmp += '<li class="day">' + day.toString("ddd, MMM d, yyyy") + "<ul>";
 
         $.each(group, function(j, ev){
             event_url = settings.src + "events/" + ev.id;
             event_link = '<a href="'+ event_url + '" target="_blank">' + ev.name +'</a>';
-            event_time = new Date(ev.start_time);
+            event_time = new Date( Date.parse( ev.start_time.replace('Z', '') ));
             out = event_link + " @ " + ev.venue_name + " (" + event_time.toString("h:mm tt") + ")";
             tmp += "<li>" + out + "</li>";
         });
@@ -72,29 +69,18 @@
       /* Bind next and previous buttons to actions */
       $(settings.selector + ' a.next_events').click( function(e){
         e.preventDefault();
-        t0 = new Date(settings.start_date);
-        t0.setDate(t0.getDate() + parseInt(settings.paginate));
-        t1 = new Date(settings.end_date);
-        t1.setDate(t1.getDate() + parseInt(settings.paginate));
-        sd = t0.getFullYear() + "-" + (t0.getMonth() + 1) + "-" + t0.getDate();
-        ed = t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate();
-        settings.start_date = t0.toString("yyyy-M-d");
-        settings.end_date = t1.toString("yyyy-M-d");
+
+        settings.start_date = settings.start_date.add(parseInt(settings.paginate)).days();
+        settings.end_date   = settings.end_date.add(parseInt(settings.paginate)).days();
         settings.events_url = build_events_url(settings);
-        console.log(settings);
 
         load_week( settings );
       });
       $(settings.selector + ' a.prev_events').click( function(e){
         e.preventDefault();
-        t0 = new Date(settings.start_date);
-        t0.setDate(t0.getDate() - settings.paginate);
-        t1 = new Date(settings.end_date);
-        t1.setDate(t1.getDate() - settings.paginate);
-        sd = t0.getFullYear() + "-" + (t0.getMonth() + 1) + "-" + t0.getDate();
-        ed = t1.getFullYear() + "-" + (t1.getMonth() + 1) + "-" + t1.getDate();
-        settings.start_date = t0.toString("yyyy-M-d");
-        settings.end_date = t1.toString("yyyy-M-d");
+
+        settings.start_date = settings.start_date.add(parseInt(settings.paginate) * -1).days();
+        settings.end_date   = settings.end_date.add(parseInt(settings.paginate) * -1).days();
         settings.events_url = build_events_url(settings);
 
         load_week( settings );
@@ -111,8 +97,8 @@
     $.each(settings, function(key, val){
       query.push("&" + key + "=" + val);  
     });
-    events_url = settings.src + "woof/events/show_by_date?" + "start_date=" + settings.start_date +
-                 "&end_date=" + settings.end_date + "&categories=" + settings.categories;
+    events_url = settings.src + "woof/events/show_by_date?" + "start_date=" + settings.start_date.toString("yyyy-M-d") +
+                 "&end_date=" + settings.end_date.toString("yyyy-M-d") + "&categories=" + settings.categories;
     return encodeURI(events_url);
   }
 
